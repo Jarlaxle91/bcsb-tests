2268ac385f4a900360505129e209dc91f24bc66f
Jarlaxle91 <Jarlaxle91@gmail.com>
2022-02-08 09:38:41 +1000

    Init project

---
+++ src/test/java/module_example/SeleniumTest.java
@@ -0,0 +1,19 @@
+package module_example;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SeleniumTest {
+
+  @ExtendWith(SeleniumJupiter.class)
+  @Test
+  void projectIsConfigured(ChromeDriver driver) {
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+    assertThat(driver.getTitle()).contains("Selenium WebDriver");
+  }
+}
